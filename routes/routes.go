fd9e006db05e416da40cb03cc057cb3bd70ab3ba
KorirMoze <korirkipsmoze@gmail.com>
2023-06-12 15:30:44 +0300

    creating routes

--- routes/routes.go
+++ routes/routes.go
@@ -1,3 +1,14 @@
 package routes
 
-import
+import (
+	"Ecommerce/controllers"
+	"github.com/gin-gonic/gin"
+)
+
+func UserRoutes(incomingRoutes *gin.Engine){
+	incomingRoutes.POST("/users/signup",controllers.SignUp())
+	incomingRoutes.POST("/users/login", controllers.login())
+	incomingRoutes.POST("/admin/addproduct", controllers.ProductViewerAmin())
+	incomingRoutes.GET("/users/productview",controllers.SearchProduct())
+	incomingRoutes.GET("/users/search", controllers.SearchProductByQuery())
+}
